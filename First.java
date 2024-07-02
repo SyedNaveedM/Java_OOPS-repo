1c6ec9d389a5d7d477b0462f34ae9bd5d9b7c1b7
SyedNaveedM <Syed.Naveed@iiitb.ac.in>
2024-07-02 18:46:30 +0530

    Syntax

---
+++ First.java
@@ -0,0 +1,16 @@
+//Here the name afte public class is the name of the file without the java extension
+//We use PascalConvention for classes
+//We use camelCaseConvention for functions
+public class hi
+{
+    //Public here is a access modifier which means that the methods can be accessed from anywhere
+    //Static allows us to use a function within a class without creating an object of it
+    //Void is the return type
+    //main is the entry point for our program
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Naveed!");
+        System.out.println("This is the first Java program!");
+        System.out.println("Third line");
+    }
+}
