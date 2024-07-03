33f8343e52719ed655bac895888a90a0cde34218
SyedNaveedM <Syed.Naveed@iiitb.ac.in>
2024-07-03 10:58:47 +0530

    Taking  input

--- input.java
+++ input.java
@@ -1,7 +1,8 @@
 //Here the name afte public class is the name of the file without the java extension
 //We use PascalConvention for classes
 //We use camelCaseConvention for functions
-public class hi
+import java.util.Scanner;
+public class input
 {
     //Public here is a access modifier which means that the methods can be accessed from anywhere
     //Static allows us to use a function within a class without creating an object of it
@@ -9,8 +10,10 @@ public class hi
     //main is the entry point for our program
     public static void main(String[] args)
     {
-        System.out.println("Hello Naveed!");
-        System.out.println("This is the first Java program!");
-        System.out.println("Third line");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int a=sc.nextInt();
+        System.out.println(2*a);
+        
     }
 }
